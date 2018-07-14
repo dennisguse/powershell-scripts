9508a63653c9951d6f30878e46ecedf0279ffb0b
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2018-07-14 23:24:54 +0200

    sqlserver-cmd-to-file: output to file and console. Removed parameter to set database - use database in SQL statement instead.

--- sqlserver-cmd-to-file.ps1
+++ sqlserver-cmd-to-file.ps1
@@ -4,8 +4,9 @@ Dennis Guse, dennis.guse@alumni.tu-berlin.de
 
 .SYNOPSIS
 
-Executes a SQL query/command, converts (single line, without seperators), and prints the results to STDOUT.
+Executes a SQL query/command, converts (single line, without seperators), and writes the results to $outFile or prints them to STDOUT.
 This script is designed for queries with one row and one column.
+It is, for example, useful to export JSON files from SQLServer.
 
 Requires the Microsoft SQLServer Powershell module: https://docs.microsoft.com/en-us/sql/powershell/sql-server-powershell?view=sql-server-2017
 #>
@@ -15,19 +16,20 @@ Param(
   [Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
   [string]$serverInstance,
   [Parameter(Mandatory=$True,Position=2,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
-  [string]$database,
-  [Parameter(Mandatory=$True,Position=3,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
-  [string]$sqlQuery
+  [string]$sqlQuery,
+  [Parameter(Mandatory=$True,Position=3,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True,
+        HelpMessage="Path to the destination file. Leave empty to pipe as 'SQLResult' and log to console.")]
+  [AllowEmptyString()]
+  [string]$outFile
 )
 
 Write-Host $MyInvocation.MyCommand.Name
 
 Write-Host $serverInstance
-Write-Host $database
 Write-Host $sqlQuery
 
 try {
-  $resultSet = Invoke-Sqlcmd -ServerInstance ($serverInstance) -Database ($database) -Query ($sqlQuery)
+  $resultSet = Invoke-Sqlcmd -ServerInstance ($serverInstance) -Query ($sqlQuery)
 } catch {
   exit -1
 }
@@ -43,8 +45,14 @@ else {
 #Convert table to string (one line)
 $resultString = [string]::Concat($resultStringArray)
 
-#Prepare pipeline content
-$pipelineOutput = New-Object –typename PSObject
-$pipelineOutput | Add-Member -membertype NoteProperty -name SQLResult -value ($resultString)
-Write-Output $pipelineOutput
+if ($outFile.Length -gt 0) {
+    $resultString | Out-File -FilePath ($outFile) -Encoding 'UTF8' -Confirm
+} else {
+    #Prepare pipeline content
+    $pipelineOutput = New-Object –typename PSObject
+    $pipelineOutput | Add-Member -membertype NoteProperty -name SQLResult -value ($resultString)
+    #Write-Output $pipelineOutput
+
+    Write-Output $pipelineOutput
+}
 
